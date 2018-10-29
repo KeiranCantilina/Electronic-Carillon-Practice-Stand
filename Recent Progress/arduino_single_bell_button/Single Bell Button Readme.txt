a2c03b0adca6b19dbff2e9d1b3f8d1e7c6e7ed8a
KeiranCantilina <keirancantilina@gmail.com>
2018-10-29 01:28:22 +0100

    Add files via upload

---
+++ Recent Progress/arduino_single_bell_button/Single Bell Button Readme.txt
@@ -0,0 +1,6 @@
+Before running, make sure that:
+
+1. loopMIDI is running with two virtual midi ports (one for in, one for out)
+2. Reaver is running with the correct VSTi
+3. The Wildfire has been loaded with the correct data
+4. Serial_MIDI blahhh has been started.
---
+++ Recent Progress/arduino_single_bell_button/arduino_single_bell_button.ino
@@ -0,0 +1,32 @@
+int buttonInput = 5;
+int buttonState = 0;
+int i = 0;
+void setup() {
+  //  Set MIDI baud rate:
+  pinMode(buttonInput, INPUT);
+  digitalWrite(buttonInput, HIGH);
+  Serial.begin(57600);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonInput);
+
+    if(buttonState != HIGH&i==0){
+    noteOn(0x90, 0x32, 0x45);
+    delay(100);
+    noteOn(0x90, 0x32, 0x00);
+    i = 1;
+    }
+    if(buttonState == HIGH){
+      i = 0;
+    }
+
+}
+
+//  plays a MIDI note.  Doesn't check to see that
+//  cmd is greater than 127, or that data values are  less than 127:
+void noteOn(int cmd, int pitch, int velocity) {
+  Serial.write(cmd);
+  Serial.write(pitch);
+  Serial.write(velocity);
+}
