772c1522901f04de6b8a3d1216c3c4de7dd36140
KeiranCantilina <keirancantilina@gmail.com>
2020-07-20 13:14:40 -0400

    Added code for using Arduino Micro to read linear Hall effect sensors

---
+++ Code/Linear_Hall_Array_Reader/Linear_Hall_Array_Reader.ino
@@ -0,0 +1,101 @@
+/*
+  Prototype code to read velocity from a large array of
+  linear Hall effect sensors. This code is meant to be used for
+  a MIDI carillon practice stand project, as a way to detect
+  baton strikes.
+
+  TO DO: Add calibration method in this script (for setting trigger values)
+*/
+
+#define N 12 // Keyboard chunk size (ie. number of notes)
+#define TOP 1 // Top note (in MIDI notation) of keyboard chunk
+#define BOTTOM 12 // Bottom note
+#define LOOP_TIME 1 // Loop delay time in ms
+#define SCALING 1 // Scaling factor for velocity
+#define CHANNEL 0x90 // MIDI channel
+
+// Global array of arrays
+int global_array[N][3];
+int pin_array[N] = {0,1,2,3,4,5,6,7,8,9,10,11};
+int MIDI_array[N] = {0};
+int trigger_values[N] = {0};
+
+void setup() {
+  // init global array
+  for (int i = 0; i<3; i++){
+    for (int j = 0; j<N; j++){
+      global_array[j][i] = 1023;
+    }
+  }
+  
+  // init midi array
+  for (int i = 0; i<N; i++){
+    MIDI_array[N] = BOTTOM+i;
+  }
+  
+  // init pins
+  for (int i = 0; i < N; i++){
+    pinMode(pin_array[i], INPUT);
+    // Dunno if pullups are needed 
+  }
+
+  // Start serial stuff
+  Serial1.begin(57600);
+}
+
+void loop() {
+  
+  // init vars
+  int data = 1024;
+  int velocity = 0;
+  int delta = 0;
+
+  // Loop through buffers
+  for (int i = 0; i<3; i++){
+    
+    // Loop through reading each sensor
+    for (int j = 0; j<N; j++){
+      data = analogRead(j);
+      global_array[j][i] = data;
+      
+      // If a sensor value is under the threshold...
+      if(data <= trigger_values[j]){
+
+        // Calculate delta using averaged buffer values
+        if (i == 2){
+          delta = ((global_array[j][2]-global_array[j][1])+(global_array[j][1]-global_array[j][0]))/2;
+        }
+        else if (i == 1){
+          delta = ((global_array[j][1]-global_array[j][0])+(global_array[j][0]-global_array[j][2]))/2;
+        }
+        else if (i == 0){
+          delta = ((global_array[j][0]-global_array[j][2])+(global_array[j][2]-global_array[j][1]))/2;
+        }
+
+        // If the delta isn't 0, map delta to velocity
+        if (delta>0){
+          velocity = delta*SCALING;
+
+          // Then play the triggered note with the measured velocity
+          playNote(MIDI_array[j], velocity);
+        }
+      }
+    }
+
+    // Wait before moving on to the next buffer (to allow time for baton to move)
+    delay(LOOP_TIME);
+  }
+}
+
+// Function for playing a carillon MIDI note (on then immediately off)
+void playNote(int note, int volume){
+  noteOn(CHANNEL, note, volume);
+  noteOn(CHANNEL, note, 0x00);
+}
+
+// Function for building and sending MIDI messages over the serial interface 
+void noteOn(int cmd, int pitch, int velocity) {
+  Serial1.write(cmd);
+  Serial1.write(pitch);
+  Serial1.write(velocity);
+}
